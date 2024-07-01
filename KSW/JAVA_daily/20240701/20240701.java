ce56a675c891dcee38fd37bd577b759b9bd87f8f
Kdreamtomaster <kang555668@gmail.com>
2024-07-01 23:44:39 +0900

    20240701 complete

---
+++ KSW/JAVA_daily/20240701/20240701.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 / num2;
+        return answer;
+    }
+}
+// 자바는 몫연산자가 따로 없어서 몫을 나타내고 싶으면  정수형으로 나눈 결과값 자료형을 정해주면 된다. 
