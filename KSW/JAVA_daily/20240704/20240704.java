23d822a198aa7084799f5174a15d3cb52af3780a
Kdreamtomaster <kang555668@gmail.com>
2024-07-04 22:19:58 +0900

    20240704 complete

---
+++ KSW/JAVA_daily/20240704/20240704.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        answer = message.length()*2;
+        return answer;
+    }
+}
+
+// 함수 안찾아보고 푼거에 일단 만족..
